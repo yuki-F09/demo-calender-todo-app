3772bf647d4c52a3ca1eefac79b8740b32fd1245
Yu_ki <yu_ki1309@outlook.jp>
2026-05-14 18:03:53 +0900

    fullCalendarの使い方練習

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -1,6 +1,34 @@
+"use client"
+
+import dayGridPlugin from "@fullcalendar/daygrid";
+import FullCalendar from "@fullcalendar/react";
+
+
+const tasks = [
+  {
+  title:"タスクA",
+  date:"2026-05-13",
+  backgroundColor: "#93c5fd"
+},
+  {
+  title:"タスクBBB",
+  date:"2026-05-14",
+  backgroundColor: "#8b5cf6"
+},
+]
+
+
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className="w-100">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          events={tasks}
+        />
+
+
+    </div>
+    
   )
 }
 

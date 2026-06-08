c818522468064b782e59ccd3cb845875599314e3
Yu_ki <yu_ki1309@outlook.jp>
2026-06-08 23:38:53 +0900

    サイドバーの導入と機能チェック

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Header } from "@/components/header";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/sidebar";
+
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
@@ -30,7 +34,15 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", figtree.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+
+      <body className="min-h-full flex flex-col">
+        <Header/>
+        <SidebarProvider>
+          <AppSidebar/>
+
+        {children}
+         </SidebarProvider>
+        </body>
     </html>
   );
 }
